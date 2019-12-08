3437940b3d1685042f2ec7b2732df108f1207399
yuuki1827 <kouaka.hibatsuna@gmail.com>
2019-12-08 20:35:08 +0900

    プロジェクト作成

---
+++ Assets/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Vector3 m_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 形状位置の保持
+        m_pos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 形状位置の更新
+        transform.localPosition = m_pos;
+        m_pos.x += 0.05f;
+    }
+}
